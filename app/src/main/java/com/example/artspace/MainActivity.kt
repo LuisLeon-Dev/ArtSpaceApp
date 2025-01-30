55d15f78436d638967c14fd7b5f90e090a0216b7
LuisLeon-Dev <lleon5073@gmail.com>
2025-01-30 14:49:02 -0600

    add: buttons functionality

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.artspace
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -22,12 +21,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,14 +50,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainView(modifier: Modifier = Modifier){
-    data class Places(val name: String, val place: String, val year: String)
+    data class Places(val name: String, val city: String, val year: String)
     val placesList: List<Places> = listOf(
         Places("The Bean", "Chicago, Illinois", "2024"),
-        Places("Monument Circle", "Indianapolis, Indiana", "2024")
+        Places("Monument Circle", "Indianapolis, Indiana", "2024"),
+        Places("Windmill Island Gardens", "Holland, Michigan", "2024")
     )
 
-    var currentIndex by remember { mutableIntStateOf(0) }
+    var currentIndex by remember { mutableIntStateOf(0)}
+    val currentPlace = placesList[currentIndex]
 
+    fun previousPlace(){
+        if(currentIndex > 0){currentIndex--}
+    }
+    fun nextPlace(){
+        if(currentIndex < placesList.size - 1){currentIndex++}
+    }
 
     Column(
         modifier = modifier
@@ -64,15 +74,23 @@ fun MainView(modifier: Modifier = Modifier){
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        ImageCard(title = "The Bean", place = "Chicago, Illinois", year = "2024" )
+        ImageCard(title = currentPlace.name, city = currentPlace.city, year = currentPlace.year )
 
         Row(
             modifier = modifier
                 .padding(16.dp)
         ){
-            ButtonComponent(onClick = { Log.d("boton","Back")}, iconImg = Icons.AutoMirrored.Default.ArrowBack)
+            ButtonComponent(
+                onClick = { previousPlace() },
+                enabled = currentIndex > 0,
+                iconImg = Icons.AutoMirrored.Default.ArrowBack)
+
             Spacer(modifier = modifier.width(32.dp))
-            ButtonComponent(onClick = {Log.d("boton","Forward") }, iconImg = Icons.AutoMirrored.Default.ArrowForward)
+
+            ButtonComponent(
+                onClick = { nextPlace() },
+                enabled = currentIndex < placesList.size - 1,
+                iconImg = Icons.AutoMirrored.Default.ArrowForward)
         }
     }
 }
@@ -81,7 +99,7 @@ fun MainView(modifier: Modifier = Modifier){
 fun ImageCard(
     modifier: Modifier = Modifier,
     title: String,
-    place: String,
+    city: String,
     year: String){
     Surface(
         modifier = modifier
@@ -93,10 +111,11 @@ fun ImageCard(
             Text(
                 text = title,
                 fontSize = 32.sp,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(lineHeight = 32.sp)
             )
             Text(
-                text = "$place ($year)"
+                text = "$city ($year)"
             )
         }
     }
@@ -104,16 +123,16 @@ fun ImageCard(
 
 @Composable
 fun ButtonComponent(
-    modifier: Modifier  = Modifier,
     onClick: () -> Unit,
-    iconImg: ImageVector
+    iconImg: ImageVector,
+    enabled: Boolean
 ){
     Button(
         onClick = onClick,
-        modifier = modifier
+        enabled = enabled
     ) {
         Icon(
-            imageVector = iconImg,
+            iconImg,
             contentDescription = null
         )
     }
